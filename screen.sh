45ce1aa69f7ea6012547e5899440f034a80677e7
patrick brisbin <pbrisbin@gmail.com>
2012-03-16 20:21:51 -0400

    drop main, not using it lately

--- screen.sh
+++ screen.sh
@@ -7,10 +7,6 @@ export SCREEN_CONF_DIR="$HOME/.screen/configs"
 # export the default configuration (used when just calling "screen")
 export SCREEN_CONF='clean'
 
-# setup some useful aliases that open alternative screens
- alias main='SCREEN_CONF=main screen -S main -D -R main'
-alias clean='SCREEN_CONF=clean screen -S clean -D -R clean'
-
-# define these only if the programs are available
+# alternate screens, defined only if the programs are available
 which irssi    &>/dev/null && alias    irssi='SCREEN_CONF=irssi screen -S irssi -R -D irssi'
 which rtorrent &>/dev/null && alias rtorrent='SCREEN_CONF=rtorrent screen -S rtorrent -R -D rtorrent'
