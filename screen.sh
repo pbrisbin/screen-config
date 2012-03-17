fb5c017a398520657e56d74e6c4cd3882f40977c
patrick brisbin <pbrisbin@gmail.com>
2012-03-16 20:20:49 -0400

    whitespace, don't alias missing programs

--- screen.sh
+++ screen.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # define our alternate screenrc location
 export SCREENRC="$HOME/.screen/screenrc"
 
@@ -10,7 +8,9 @@ export SCREEN_CONF_DIR="$HOME/.screen/configs"
 export SCREEN_CONF='clean'
 
 # setup some useful aliases that open alternative screens
-alias main='SCREEN_CONF=main screen -S main -D -R main'
+ alias main='SCREEN_CONF=main screen -S main -D -R main'
 alias clean='SCREEN_CONF=clean screen -S clean -D -R clean'
-alias rtorrent='SCREEN_CONF=rtorrent screen -S rtorrent -R -D rtorrent'
-alias irssi='SCREEN_CONF=irssi screen -S irssi -R -D irssi'
+
+# define these only if the programs are available
+which irssi    &>/dev/null && alias    irssi='SCREEN_CONF=irssi screen -S irssi -R -D irssi'
+which rtorrent &>/dev/null && alias rtorrent='SCREEN_CONF=rtorrent screen -S rtorrent -R -D rtorrent'
